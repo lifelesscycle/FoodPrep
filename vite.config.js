dbcbcabaa5266c61511c5f62da959880187c1fa7
lifelesscycle <aaryanema2004@gmail.com>
2025-06-09 00:18:26 +0530

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:process.env.VITE_BASE_PATH || "/FoodPrepVIT/tree/main/FoodPrepVIT"
+  base:process.env.VITE_BASE_PATH || "/FoodPrep"
 })
