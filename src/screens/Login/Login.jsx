738d7fd3f399795fe83ed1b997050bbcac246854
lifelesscycle <91150189+lifelesscycle@users.noreply.github.com>
2025-07-02 14:51:19 +0530

    Update Login.jsx

--- src/screens/Login/Login.jsx
+++ src/screens/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = ({ isOpen, onClose, onLogin }) => {
     setError('');
 
     try {
-      const response = await fetch('https://foodprepbackend-crj8.onrender.com/api/auth/login', {
+      const response = await fetch('https://foodprep-1w7j.onrender.com/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
